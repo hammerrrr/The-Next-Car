e507fa801a742b0c9fb3b18a983e533234e3af48
Ivan Nur Ilham Syah <ivannurilham60@hotmail.com>
2020-10-28 03:07:55 +0700

    add accubattery handle func

--- The Next Car/Model/AccuBattery.cs
+++ The Next Car/Model/AccuBattery.cs
@@ -6,5 +6,27 @@ namespace The_Next_Car.Model
 {
     class AccuBattery
     {
+        private int voltage;
+        private Boolean stateOn = false;
+
+        public AccuBattery (int voltage)
+        {
+            this.voltage = voltage;
+        }
+
+        public void turnOn ()
+        {
+            this.stateOn = true;
+        }
+
+        public void turnOff ()
+        {
+            this.stateOn = false;
+        }
+
+        public Boolean isOn ()
+        {
+            return this.stateOn;
+        }
     }
 }
