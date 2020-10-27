aee38413691a3aa3b21787f888ab14ae187b3e74
Ivan Nur Ilham Syah <ivannurilham60@hotmail.com>
2020-10-28 03:09:54 +0700

    add accubattery controller

--- The Next Car/Controller/AccuBatteryController.cs
+++ The Next Car/Controller/AccuBatteryController.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using The_Next_Car.Model;
 
 namespace The_Next_Car.Controller
 {
     class AccuBatteryController
     {
+        private AccuBattery accuBattery;
+        private OnPowerChanged callBackOnPowerChanged;
+
+        public AccuBatteryController (OnPowerChanged callBackOnPowerChanged)
+        {
+            this.accuBattery = new AccuBattery(12);
+            this.callBackOnPowerChanged = callBackOnPowerChanged;
+        }
+
+        public bool accuBatteryIsOn ()
+        {
+            return this.accuBattery.isOn();
+        }
+
+        public void turnOn ()
+        {
+            this.accuBattery.turnOn();
+            this.callBackOnPowerChanged.onPowerChangedStatus("ON", "power is on");
+        }
+        public void turnOff()
+        {
+            this.accuBattery.turnOff();
+            this.callBackOnPowerChanged.onPowerChangedStatus("OFF", "power is off");
+        }
+
+    }
+
+    interface OnPowerChanged 
+    {
+        void onPowerChangedStatus(string value, string message);
     }
 }
