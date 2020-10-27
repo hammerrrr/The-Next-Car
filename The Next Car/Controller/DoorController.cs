1e9c58fe3c86338089c237de43099b8b3ef7f7a9
Ivan Nur Ilham Syah <ivannurilham60@hotmail.com>
2020-10-28 03:25:12 +0700

    add door controller

--- The Next Car/Controller/DoorController.cs
+++ The Next Car/Controller/DoorController.cs
@@ -1,10 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using The_Next_Car.Model;
 
 namespace The_Next_Car.Controller
 {
     class DoorController
     {
+        private Door door;
+        private OnDoorChanged onDoorChanged;
+
+        public DoorController (OnDoorChanged onDoorChanged)
+        {
+            this.door = new Door();
+            this.onDoorChanged = onDoorChanged;
+        }
+
+        public void close ()
+        {
+            this.door.close();
+            this.onDoorChanged.doorSecurityChanged("CLOSED", "door is closed");
+        }
+
+        public void open()
+        {
+            this.door.open();
+            this.onDoorChanged.doorSecurityChanged("OPENED", "door is opened");
+        }
+
+        public void activateLock ()
+        {
+            if (this.door.isClosed ())
+            {
+                this.door.activateLock();
+                onDoorChanged.doorSecurityChanged("LOCKED", "door is locked");
+            } else
+            {
+                unlock();
+            }
+        }
+
+        public void unlock ()
+        {
+            this.door.unlock();
+            onDoorChanged.doorSecurityChanged("UNLOCKED", "door is unlocked");
+        }
+
+        public bool isClosed ()
+        {
+            return this.door.isClosed();
+        }
+        public bool isLocked()
+        {
+            return this.door.isLocked ();
+        }
+    }
+
+    interface OnDoorChanged
+    {
+        void doorSecurityChanged(string vale, string message);
+        void doorStatusChanged(string vale, string message);
     }
 }
