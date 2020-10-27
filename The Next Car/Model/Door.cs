521d99339be51196e7f17ff4930fee41006e080d
Ivan Nur Ilham Syah <ivannurilham60@hotmail.com>
2020-10-28 03:08:07 +0700

    add door handle func

--- The Next Car/Model/Door.cs
+++ The Next Car/Model/Door.cs
@@ -6,5 +6,37 @@ namespace The_Next_Car.Model
 {
     class Door
     {
+        public bool closed;
+        public bool locked;
+
+        public void close ()
+        {
+            this.closed = true;
+        }
+
+        public void open ()
+        {
+            this.closed = false;
+        }
+
+        public void activateLock ()
+        {
+            this.locked = true;
+        }
+
+        public void unlock()
+        {
+            this.locked = false;
+        }
+
+        public bool isLocked ()
+        {
+            return this.locked;
+        }
+
+        public bool isClosen ()
+        {
+            return this.closed;
+        }
     }
 }
