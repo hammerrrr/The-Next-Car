1428aa06fdaf0a11ed83bf7810d86c15c2f525a4
Ivan Nur Ilham Syah <ivannurilham60@hotmail.com>
2020-10-28 09:32:12 +0700

    add instance of each controller

--- The Next Car/MainWindow.xaml.cs
+++ The Next Car/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using The_Next_Car.Controller;
 
 namespace The_Next_Car
 {
@@ -20,29 +21,60 @@ namespace The_Next_Car
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Car nextCar;
         public MainWindow()
         {
             InitializeComponent();
+
+            AccuBatteryController accuBatteryController = new AccuBatteryController(this);
+            DoorController doorController = new DoorController(this);
+
+            nextCar = new Car (accuBatteryController, doorController, this);
+        }
+
+        private void carEngineStatusChanged (string val, string msg)
+        {
+            Status.Content = msg;
+            StartButton.Content = val;
+        }
+
+        private void doorSecurityChanged(string val, string msg)
+        {
+            this.LockDoorState.Content = msg;
+            this.LockDoorButton.Content = val;
+        }
+
+        private void doorStatusChanged(string val, string msg)
+        {
+            this.DoorOpenState.Content = msg;
+            this.DoorOpenButton.Content = val;
+        }
+
+        private void onPowerChangedStatus(string val, string msg)
+        {
+            this.AccuState.Content = msg;
+            this.AccuButton.Content = val;
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("start button");
+            this.nextCar.toggleStartEngineButton ();
         }
 
         private void DoorOpenButton_Click(object sender, RoutedEventArgs e)
-        {
-            Console.WriteLine("door open button");
+        { 
+            this.nextCar.toggleTheDoorButton ();
         }
 
-        private void LoockDoorButton_Click(object sender, RoutedEventArgs e)
+        private void LockDoorButton_Click(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("lock door button");
+            this.nextCar.toggleTheLockDoorButton ();
         }
 
         private void AccuButton_Click(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("accu button");
+            this.nextCar.toggleThePowerButton ();
         }
+
     }
 }
